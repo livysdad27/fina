a0351f00cd0743c9d946351fcce95f9993601843
livysdad27 <livysdad27@gmail.com>
2016-11-08 11:49:05 -0800

    added coverage for finaUpdt

--- finaLib/finaUpdt.py
+++ finaLib/finaUpdt.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+import finaImp as fi
 import os, pandas
 
-def updateCat(payeeName, catName, dFrame):
+def updateCat(payeeName, catName, dFrame, dirName="data", pklName="master.pkl"):
   '''Update a category and re-persist the DataFrame to pickle again'''
   dFrame.loc[dFrame['payee'] == payeeName, 'cat'] = catName
-  dFrame.to_pickle(os.path.join("data", "master.pkl"))
+  fi.pickleDataFrame(dFrame, pklName, dirName)
+  return True
---
+++ tests/test_finaUpdt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from finaLib import finaDisp
+from finaLib import finaImp 
+from finaLib import finaExp
+from finaLib import finaUpdt
+import unittest, os, pandas, shutil, cStringIO
+
+class testImport(unittest.TestCase):
+  '''Tests for updating cats''' 
+
+  def setUp(self):
+    pass
+ 
+  def tearDown(self):
+    pass
+
+  def testUpdateCat(self):
+    '''******Test that we can update a cat'''
+    df = finaExp.unPickleData("test.pkl", "tests/assets")
+    odf, ostr = finaDisp.dispOFX(df)
+    self.assertTrue(finaUpdt.updateCat('toe', 'jamb', odf, "tests/assets", "test.pkl"))
+
